ae1201f55233365bac8dd34e084663beb2f39808
romina888 <romina_albornoz_88@hotmail.com>
2025-10-23 01:37:15 -0300

    Correccion al nombre de la ruta useCartContent

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useCartContext } from "../../context/CardContext/UseCartContext";
+import { useCartContext } from "../../context/CardContext/useCartContext";
 import "./Nav.css";
 
 export const Nav = () => {
